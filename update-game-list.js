7f15e011e2bdc5523876eb2e536268963eb5c099
boyindian <boyindia11@gmail.com>
2024-09-24 12:30:21 +0800

    chore: create game generate script

--- update-game-list.js
+++ update-game-list.js
@@ -8,11 +8,14 @@ const currentDir = process.cwd();
 const gameDirectories = fs.readdirSync(currentDir, { withFileTypes: true })
   .filter(dirent => dirent.isDirectory())
   .map(dirent => dirent.name)
-  .filter(name => name !== 'node_modules'); // 排除 node_modules 目录
+  .filter(name => name !== 'node_modules' && name !== '.git' && name !== '.github'); // 排除 node_modules, .git 和 .github 目录
 
 // 读取 index.html 文件
 const indexPath = path.join(currentDir, 'index.html');
 let indexContent = fs.readFileSync(indexPath, 'utf-8');
+
+// 删除所有 class="game-item" 的 div 元素
+indexContent = indexContent.replace(/<div class="game-item">.*?<\/div>/g, '');
 
 // 生成新的游戏列表 HTML
 const gameListHtml = gameDirectories.map(game => {
@@ -20,7 +23,7 @@ const gameListHtml = gameDirectories.map(game => {
   return `        <div class="game-item"><a href="${game}/">${gameTitle}</a></div>`;
 }).join('\n');
 
-// 替换 index.html 中的游戏列表
+// 在 game-list div 中插入新的游戏列表
 const regex = /(<div class="game-list">)([\s\S]*?)(<\/div>)/;
 const newContent = indexContent.replace(regex, `$1\n${gameListHtml}\n    $3`);
 
